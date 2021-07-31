d41ce3b4a70f68eabb73808bac564d71428671ab
Proximamusic <sabfit3@gmail.com>
2021-07-31 23:23:11 +0530

    done with mobile dropdown navbar and complete mobile design

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 // selectors
 const accordionBtn = document.querySelectorAll('.question');
 const pTag  = document.querySelectorAll('.faq-accordion p');
+const dropDown = document.querySelector('.navbar-dropdown');
+const dropdownContent = document.querySelector('.dropdown-content');
+const burger = document.querySelector('.burger');
+const closing = document.querySelector('.close');
 
 // Event listeners
 
 accordionBtn.forEach(e => {
     e.addEventListener('click' , showAnswer);
 })
+
+burger.onclick = function(){
+    dropDown.style.display = 'flex';
+    setTimeout(()=>{dropDown.classList.add('dropdown-animation');
+    dropdownContent.style.opacity = '1';
+}, 100)
+    
+}
+closing.onclick = function(){
+    dropDown.classList.remove('dropdown-animation');
+    setTimeout(()=>{dropDown.style.display = 'none';
+    dropdownContent.style.opacity = '0';
+}, 400)
+}
 
 // functions
 
